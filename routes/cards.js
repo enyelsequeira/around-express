f35b856c112d5cc08f16ed5e87cadb0090ead0c5
enyelsequeira <enyelsequeira@hotmail.com>
2020-11-13 11:07:30 +0000

    testing end point

--- routes/cards.js
+++ routes/cards.js
@@ -10,10 +10,10 @@ const {
 
 const router = express.Router();
 // router === /cards, (logic)
-router.get('cards', getCards);
-router.post('cards', createCard);
-router.delete('cards/:cardId', deleteCard);
-router.put('cards/likes/:id', likeCard);
-router.delete('cards/likes/:id', deleteCardLike);
+router.get('/cards', getCards);
+router.post('/cards', createCard);
+router.delete('/cards/:cardId', deleteCard);
+router.put('/cards/likes/:id', likeCard);
+router.delete('/cards/likes/:id', deleteCardLike);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -13,10 +13,10 @@ const auth = require('../middleware/auth');
 router.get('/users/me', auth, getUserInfo);
 
 // general user routes
-router.get('users', auth, getUsers);
-router.get('users/:id', auth, getOneUser);
-router.patch('users/me', auth, updateProfile);
-router.patch('users/me/avatar', auth, updateAvatar);
+router.get('/users', auth, getUsers);
+router.get('/users/:id', auth, getOneUser);
+router.patch('/users/me', auth, updateProfile);
+router.patch('/users/me/avatar', auth, updateAvatar);
 
 module.exports = router;
 // http://localhost:3000/users/5f845d006c7b26589c33b82
